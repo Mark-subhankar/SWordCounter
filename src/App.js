13e2430f0c031f06ae7034d6dfc8b9c76a04c5d8
mark subhankar <subhankardas7044444@gmail.com>
2024-09-12 14:22:21 +0530

    wordcounter add

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Navbar from "./Component/Navbar";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import TextForm from "./Component/TextForm";
+import About from "./Component/About";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import Alert from "./Alert";
+import { useState } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    const [alert, setAlert] = useState(null);
 
-export default App;
+    const showAlert = (message, type) => {
+        setAlert({
+            msg: message,
+            type: type,
+        });
+        setTimeout(() => {
+            setAlert(null);
+        }, 1500);
+    };
+    return ( <
+        >
+        <
+        Router >
+        <
+        Navbar / >
+        <
+        Alert alert = { alert }
+        /> <ToastContainer / >
+        <
+        Routes >
+        <
+        Route exact path = "/"
+        element = { < TextForm showAlert = { showAlert }
+            />} / > { " " } <
+            Route exact path = "/about"
+            element = { < About / > }
+            />{" "} <
+            /Routes>{" "} <
+            /Router>{" "} <
+            />
+        );
+    }
+
+    export default App;
